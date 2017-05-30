d4d5c9657b072174416eafc5cbf4f8b4c1141527
havrylyshyn <bohdan.havrylyshyn@infopulse.com>
2017-05-30 11:38:20 +0300

    Package for my first program is created

--- sandbox/src/main/java/en/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/en/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
+package en.stqa.pft.sandbox;
+
 public class MyFirstProgram {
 public static void main(String[] args) {
-System.out.println("Hello world!");
+System.out.println("Hello, world!");
 }
 }
