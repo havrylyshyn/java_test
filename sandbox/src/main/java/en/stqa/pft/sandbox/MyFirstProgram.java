f9242f14ae3fbd2e0c9189aa81593450f086a70c
havrylyshyn <bohdan.havrylyshyn@infopulse.com>
2017-05-30 15:04:22 +0300

    Some functions are added

--- sandbox/src/main/java/en/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/en/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,20 @@ package en.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("Vasia");
+        hello("Petia");
+        double a = 5;
+        double b = 4;
+        System.out.println("Quadrate square with edge " + a + " = " + area(a));
+        System.out.println("Rectangle square with edges " + a + " & " + b + " = " + area(a, b));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(double a) {
+        return a*a;
+    }
+    public static double area(double a, double b) {
+        return a*b;
     }
 }
