9b948da8e1191143b27ebc23656f94d66510cd4e
havrylyshyn <bohdan.havrylyshyn@infopulse.com>
2017-06-02 14:13:58 +0300

    add new steps to GoogleSearch Class

--- addressbook-web-tests/src/test/java/en/stqa/pft/addressbook/GoogleSearch.java
+++ addressbook-web-tests/src/test/java/en/stqa/pft/addressbook/GoogleSearch.java
@@ -16,11 +16,12 @@ import static org.openqa.selenium.OutputType.*;
 
 public class GoogleSearch {
     FirefoxDriver wd;
-    
+
     @BeforeMethod
     public void setUp() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "D:\\JAVA\\geckodriver\\geckodriver.exe"); //set path to geckodriver
         wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     
     @Test
@@ -31,6 +32,7 @@ public class GoogleSearch {
         wd.findElement(By.id("lst-ib")).sendKeys("istqb ua");
         wd.findElement(By.id("_fZl")).click();
         wd.findElement(By.linkText("Ukrainian Software Quality Board")).click();
+        wd.findElement(By.linkText("Экзамены и события")).click();
     }
     
     @AfterMethod
